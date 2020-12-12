6fbe81c16bb1536a2a20ee7039a6c5c2a030890e
shreyansh978 <74063740+shreyansh978@users.noreply.github.com>
2020-12-12 20:28:46 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper{
+ constructor(x,y){
+ var options = {
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
+            'isStatic':false
+        }
+    this.body = Bodies.circle(x, y, 5, options);
+    World.add(world, this.body);
+ }
+ display(){
+        var angle = this.body.angle;
+        push();
+        fill('red');
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(CENTER);
+        ellipse(20,20);
+        pop();
+
+ }
+}
